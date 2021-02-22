e82d8b3b38cd55395e4f12371e5adc074044fe4c
marcrowland <marcrowland@outlook.com>
2021-02-22 22:10:46 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+//Moves txt files from one directory to another
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceDirectory = @"current";
+            string archiveDirectory = @"archive";
+
+            try
+            {
+                var txtFiles = Directory.EnumerateFiles(sourceDirectory, "*.txt");
+
+                foreach (string currentFile in txtFiles)
+                {
+                    string fileName = currentFile.Substring(sourceDirectory.Length + 1);
+                    Directory.Move(currentFile, Path.Combine(archiveDirectory, fileName));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
